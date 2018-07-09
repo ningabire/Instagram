89785f95ecd2358b240c3b206546114d218ae3b1
Norette Ingabire <noretteingabire@fb.com>
2018-07-09 15:44:42 -0700

    initial README

---
+++ Instagram/Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Norette Ingabire on 7/9/18.
+//  Copyright © 2018 Norette Ingabire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instagram/Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Norette Ingabire on 7/9/18.
+//  Copyright © 2018 Norette Ingabire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
